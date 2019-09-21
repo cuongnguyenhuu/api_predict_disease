2e5b970794b02cbc560673045ab267c487e8b8a7
Cuong (Currie) H.NGUYEN <cuongnguyenhuu97@gmail.com>
2019-09-21 16:10:29 +0700

    fixed cors

--- api_predict_disease/settings.py
+++ api_predict_disease/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tensorflow',
     'rest_framework',
-    'Predict'
+    'Predict',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
